eb53502d382e2db5f4ff4b2362b25a3ce47605ed
rcprins <139208563+rprins-digisteel@users.noreply.github.com>
2025-09-17 18:41:50 +0200

    Auto-generated commit

--- chunk-LUFHKT5I.js
+++
@@ -1,1 +0,0 @@
-import{Ca as a}from"./chunk-5ACEPQNJ.js";var s=class{};var r=class t{task;static \u0275fac=function(e){return new(e||t)};static \u0275dir=a({type:t,inputs:{task:"task"}})};export{s as a,r as b};
