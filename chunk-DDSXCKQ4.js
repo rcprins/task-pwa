d25f05550bee0fa8da71a53914de5de3785ddb00
rcprins <139208563+rprins-digisteel@users.noreply.github.com>
2025-09-19 16:31:35 +0200

    Auto-generated commit

---
+++ chunk-DDSXCKQ4.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./chunk-RLSUENAT.js";var s=class{};var r=class t{task;static \u0275fac=function(e){return new(e||t)};static \u0275dir=a({type:t,inputs:{task:"task"}})};export{s as a,r as b};
