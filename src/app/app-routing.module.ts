d427a5016bda59df8e41976954db027727568af9
René Prins <rcprins@gmail.com>
2025-09-30 10:57:11 +0200

    callback component removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CallbackComponent } from './auth/callback.component';
 
 const routes: Routes = [
   { path: 'callback', component: AppComponent  }, // redirect target
   { path: '', component: AppComponent },          // default
-  // { path: '**', redirectTo: '' } 
 ];
 
 @NgModule({
