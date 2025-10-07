711cef3ad4e155309b328e7901d25e0ea34cf82d
René Prins <r.prins@voortman.net>
2025-10-07 13:20:21 +0200

    Auto-generated commit

---
+++ chunk-RC5GM236.js
@@ -0,0 +1,1 @@
+import{bb as a}from"./chunk-UZTLWD75.js";var s=class{};var r=class t{task;static \u0275fac=function(e){return new(e||t)};static \u0275dir=a({type:t,inputs:{task:"task"}})};export{s as a,r as b};
